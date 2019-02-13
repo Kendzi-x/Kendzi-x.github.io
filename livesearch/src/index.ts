4ef8d658e25c0c30f97d08a385789e553f31cae3
Yuri Stepanov <stepanoff.yura@ya.ru>
2019-02-13 17:10:02 +0300

    search refactored; final version

--- livesearch/src/index.ts
+++ livesearch/src/index.ts
@@ -1,55 +1,66 @@
-import { fromEvent, Observable, of } from 'rxjs';
-import { map, debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+import { fromEvent, Observable, of, SchedulerLike } from 'rxjs';
+import {
+  map,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  catchError,
+  pluck
+} from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
+import { async } from 'rxjs/internal/scheduler/async';
 
-interface IOptions {
-  name?: string;
-  html_url?: string;
-  stargazers_count?: number;
-}
+import { ISearchOptions } from './searchOptions.interface';
 
 const el: HTMLInputElement = document.querySelector('.live-search') as HTMLInputElement;
-
-const sequence$: Observable<Object[]> = fromEvent(el, 'input').pipe(
-  debounceTime(200),
-  map((e: Event) => (e.target as HTMLInputElement).value),
-  distinctUntilChanged(),
-  switchMap((value: string) => search(value).pipe(
-    catchError(() => {
-      return of([]);
-    })))
-);
+const searchInputSequence$: Observable<Event> = fromEvent(el, 'input');
 
 const div: HTMLElement = document.getElementById('list');
 
-sequence$.subscribe((ev: Object[]) => {
-  div.innerHTML = '';
-  if (ev === undefined) {
-    return;
-  }
-  const ul: HTMLUListElement = document.createElement('ul');
-  div.appendChild(ul);
+function search(
+  source1$: Observable<Event>,
+  sourceCreator: (value: string) => Observable<Object[]>,
+  schedule: SchedulerLike = async
+): Observable<Object[]> {
+  return source1$.pipe(
+    debounceTime(500, schedule),
+    pluck('target', 'value'),
+    distinctUntilChanged(),
+    switchMap((value: string) =>
+      sourceCreator(value).pipe(
+        catchError(() => of([]))
+        )
+    )
+  );
+}
 
-  console.log(ev);
-
-  for (let i: number = 0; i < ev.length; i++) {
-    const li: HTMLLIElement = document.createElement('li');
-    const elem: IOptions = ev[i];
-    li.innerHTML = `<a href="${elem.html_url}" target="_blank">
-    ${elem.name} with (${elem.stargazers_count} star(s))</a>`;
-    ul.appendChild(li);
-  }
-}, () => {}, () => {
-  console.log('completed');
-});
-
-function search(term: string): Observable<Object[]> {
+function searchAjax(term: string): Observable<Object[]> {
   if (term === '') {
     return of([]);
   }
 
   return ajax(`https://api.github.com/search/repositories?q=${term}`).pipe(
-      // tslint:disable-next-line:no-any
-      map((r: any) => r.response.items)
-    );
+    // tslint:disable-next-line:no-any
+    map((r: any) => r.response.items)
+  );
 }
+
+function render(ev: Object[]): void {
+  div.innerHTML = '';
+    if (ev === undefined || ev.length === 0) {
+      return;
+    }
+    const ul: HTMLUListElement = document.createElement('ul');
+    div.appendChild(ul);
+
+    for (let i: number = 0; i < ev.length; i++) {
+      const li: HTMLLIElement = document.createElement('li');
+      const elem: ISearchOptions = ev[i];
+      li.innerHTML = `<a href="${elem.html_url}" target="_blank">
+    ${elem.name} with (${elem.stargazers_count} star(s))</a>`;
+      ul.appendChild(li);
+    }
+}
+
+search(searchInputSequence$, searchAjax)
+  .subscribe((result: Object[]) => render(result));
